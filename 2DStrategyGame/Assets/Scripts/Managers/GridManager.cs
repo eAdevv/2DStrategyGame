109af9bdb52a69b7a5cdd7f72f66ee848a3b953f
Emin Arslan <arslanemin26@gmail.com>
2024-08-29 05:32:14 +0300

    Build Fix

--- 2DStrategyGame/Assets/Scripts/Managers/GridManager.cs
+++ 2DStrategyGame/Assets/Scripts/Managers/GridManager.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.UIElements;
+
 
 public class GridManager : MonoSingleton<GridManager>
 {
--- 2DStrategyGame/Assets/Scripts/UnitControl/SoldierSpawnController/SoldierSpawnController.cs
+++ 2DStrategyGame/Assets/Scripts/UnitControl/SoldierSpawnController/SoldierSpawnController.cs
@@ -1,6 +1,5 @@
-using System.Collections;
+
 using System.Collections.Generic;
-using UnityEditor.Searcher;
 using UnityEngine;
 using System;
 
